baa2b8b4f1ca3e985369710effdfc413eea8c89e
Logic <88998860+LogicPenguins@users.noreply.github.com>
2025-07-31 22:23:22 +0600

    Update testing.py

--- archives/testing.py
+++ archives/testing.py
@@ -1,12 +1,7 @@
 import math
 
-# Low Earth Orbit delta-v requirement in meters per second
 DELTA_V_LEO = 9400
-
-# Vacuum specific impulse (Isp) for a Kerosene/LOX engine in seconds
 ISP_VAC = 350
-
-# Standard gravity in meters per second squared
 G0 = 9.80665
 
 mass_ratio = math.exp(DELTA_V_LEO / (ISP_VAC * G0))
